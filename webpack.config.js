a2bf0c7b9e7a1283c68b4c21c599e02dae67f38f
吴玉众 <yuzhong.wu@ucarinc.com>
2019-12-09 10:50:03 +0800

    optimaize webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const path = require('path')
 const webpack = require('webpack')
+const OpenBrowserPlugin = require('open-browser-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+const HOST = '127.0.0.1'
+const PORT = 3000
 
 module.exports = {
   entry: './src/index.js',
@@ -16,19 +23,50 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.less$/,
+        use: ["style-loader", 'css-loader', "postcss-loader", 
+          {loader: "less-loader", options: {javascriptEnabled: true}}
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|JPG|GIF|PNG|BMP|bmp|JPEG|jpeg)$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8192
+          }
+        }]
+      },
+      {
+        test: /\.(eot|woff|ttf|woff2|svg)$/,
+        use: 'url-loader'
+      },
     ]
   },
   resolve: { extensions: ["*", '.js', '.jsx']},
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/',
+    publicPath: '/dist',
     filename: 'bundle.js',
   },
   devServer: {
     contentBase: path.join(__dirname, 'public'),
-    port: 3000,
-    publicPath: 'http://localhost:3000/dist/',
+    port: PORT,
+    publicPath: `http://${HOST}:${PORT}/dist/`,
     hotOnly: true,
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new OpenBrowserPlugin({
+      url: `http://${HOST}:${PORT}`
+    }),
+    new ProgressBarPlugin(),
+    // new HtmlWebpackPlugin({
+    //   title: 'react demo',
+    //   showErrors: true,
+    // }),
+    new CleanWebpackPlugin(),
+  ],
 }
